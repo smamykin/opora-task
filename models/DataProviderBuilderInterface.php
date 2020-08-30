42572f1d9d8568a78c0350fd6ea87de4454c19d8
Sergei Mamykin <mamykin@oborot.ru>
2020-08-31 00:30:24 +0300

    10 | чтобы сократить возможность опечатки

--- models/DataProviderBuilderInterface.php
+++ models/DataProviderBuilderInterface.php
@@ -6,5 +6,11 @@ use yii\data\SqlDataProvider;
 
 interface DataProviderBuilderInterface
 {
+    public const SORT_QUERY_VAR_10D = '10days';
+    public const SORT_QUERY_VAR_3D = '3days';
+    public const SORT_QUERY_VAR_ALL = 'default';
+    public const SORT_QUERY_VAR_2H = '2hours';
+    public const SORT_QUERY_VAR_5D = '5days';
+
     public function build(string $sort): SqlDataProvider;
 }
