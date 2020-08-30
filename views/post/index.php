3db60124405254e4044943ad6590aa73c92c20be
Sergei Mamykin <mamykin@oborot.ru>
2020-08-30 22:22:14 +0300

    8 | чтобы были ссылки с сортировкой

--- views/post/index.php
+++ views/post/index.php
@@ -5,6 +5,7 @@ use yii\widgets\ListView;
 
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
+/* @var $sorter \yii\data\Sort */
 
 $this->title = 'Posts';
 $this->params['breadcrumbs'][] = $this->title;
@@ -17,7 +18,10 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('Create Post', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
-
+    <? foreach ([ '2hours', '3days', '5days', '10days', 'default', ] as $i => $order) { ?>
+        <?= $i ? '|' : ''?>
+        <?= $sorter->link($order); ?>
+    <?}?>
     <?= ListView::widget([
         'dataProvider' => $dataProvider,
         'itemOptions' => ['class' => 'item'],
