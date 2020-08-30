912be621e121f0c282f01ec3a9bd7d5dd21e67b7
Sergei Mamykin <mamykin@oborot.ru>
2020-08-30 16:05:31 +0300

    4 | чтобы работало подключение к бд в тестах

--- config/test_db.php
+++ config/test_db.php
@@ -1,6 +1,6 @@
 <?php
 $db = require __DIR__ . '/db.php';
 // test database! Important not to run tests on production or development databases
-$db['dsn'] = 'mysql:host=localhost;dbname=test_opora_task';
+$db['dsn'] = 'mysql:host=127.0.0.1;port=3306;dbname=test_opora_task';
 
 return $db;
