152744ae8604636dc6c9534c24659cb6ed3ab4b1
Sergei Mamykin <mamykin@oborot.ru>
2020-08-30 16:06:31 +0300

    4 | чтобы был тест для crud операций над постом

--- tests/acceptance/CrudPostCest.php
+++ tests/acceptance/CrudPostCest.php
@@ -13,7 +13,33 @@ class CrudPostCest
 
     public function seePage(AcceptanceTester $I)
     {
-        $I->amOnPage(Url::toRoute(['/post/index', 'id' => 1]));
-        $I->see('Пост' );
+        $I->amOnPage(Url::toRoute(['/post/index']));
+        $I->see('Posts');
+        $I->wantTo('Create new post');
+        $I->click('Create Post');
+        $I->see('Create Post');
+        $title = 'title ' . uniqid();
+        $text = 'text ' . uniqid();
+        $I->fillField('Post[title]', $title);
+        $I->fillField('Post[text]', $text);
+        $I->selectOption('Post[author_id]', 'Добавить нового');
+        $I->fillField('Post[author_name]', 'Петрович');
+        $I->click('Save');
+        $I->wait(1);
+        $I->click('Update');
+        $postId = $I->grabFromCurrentUrl('#&id=(\d+)#');
+        $I->seeInField('Post[title]', $title);
+        $I->seeInField('Post[text]', $text);
+        $authorId = $I->grabValueFrom('Post[author_id]');
+        $I->see('Петрович', "option[value='$authorId']");
+        $newTitle = $title . '2';
+        $newText = $text . '2';
+        $I->fillField('Post[title]', $newTitle);
+        $I->fillField('Post[text]', $newText);
+        $I->click('Save');
+        $I->wait(1);
+        $I->seeInSource("<td>$newText</td>");
+        $I->seeInSource("<td>$newText</td>");
+        $I->seeInSource("<td>Петрович</td>");
     }
 }
